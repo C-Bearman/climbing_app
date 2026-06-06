0ee05aaac394d7274f57e9c597a46ed3adda8592
Callum <cbearman1995@gmail.com>
2026-06-06 22:05:28 +0100

    Trying something.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'sendit-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['/', '/index.html',  '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
